6425a0a937576f7c08cc8b433513cf15b10b5fea
horvathg13 <horvathgabor338@gmail.com>
2023-11-20 22:38:35 +0100

    register JS dev

--- src/Register/register.jsx
+++ src/Register/register.jsx
@@ -1,6 +1,32 @@
+import { useState } from 'react';
 import './register.css';
 import { FaArrowCircleRight } from "react-icons/fa";     
 const Register = () => {
+
+    const [fname, setFname]=useState('');
+    const [fnameError, setFnameError]=useState(false);
+    const [lname, setLname]=useState('');
+    const [lnameError, setLnameError]=useState(false);
+    const [email, setEmail]=useState('');
+    const [emailError, setEmailError]=useState(false);
+    const [password, setPassword]=useState('');
+    const [passwordError, setPasswordError]=useState(false);
+    const [cpassword, setCPassword]=useState('');
+    const [cpasswordError, setCPasswordError]=useState(false);
+    const [errors, setErrors]=useState([]);
+
+    const sendRegister=(event)=>{
+        event.preventDefault();
+        
+        if(password != cpassword){
+            setCPasswordError(true);
+            setPasswordError(true);
+            setErrors('Passwords does not match');
+        }
+        //api call, error handle component, success component
+    }
+
+
     return (
         <div className="reg-container flex">
             <div className="left-container"></div>
@@ -9,31 +35,30 @@ const Register = () => {
                     <h2>Registration</h2>
                 </div>
                 <div className="form-container">
-                    <form>
+                    <form onSubmit={sendRegister}>
                         <div className="name-fields flex">
                             <div className="first-name">
                                 <label>First Name</label>
-                                <input type="text" require />
-                                
+                                <input className={fnameError ? 'InputError':null} type="text" required onChange={(e)=>{setFname(e.target.value)}}/>
                             </div>
                             <div className="last-name">
                                 <label>Last Name</label>
-                                <input type="text" require />
+                                <input className={lnameError ? 'InputError':null} type="text" required onChange={(e)=>{setLname(e.target.value)}}/>
                             </div>
                             
                         </div>
                         <div className="emailPassword-fields grid">
                             <label>Email</label>
-                            <input type="email" require/>
+                            <input className={emailError ? 'InputError':null} type="email" required onChange={(e)=>{setEmail(e.target.value)}}/>
                             
                             <label>Password</label>
-                            <input type="password" require/>
+                            <input className={passwordError ? 'InputError':null} type="password" required onChange={(e)=>{setPassword(e.target.value)}}/>
                             
                             <label>Confirm Password</label>
-                            <input type="password" require/>
+                            <input className={cpasswordError ? 'InputError':null}type="password" required onChange={(e)=>{setCPassword(e.target.value)}}/>
                         </div>
                         
-                        <button className='btn'>Send <FaArrowCircleRight className='btn-icon'/></button>
+                        <button type='submit' className='btn'>Send <FaArrowCircleRight className='btn-icon'/></button>
                     </form>
                 </div>
             </div>            
