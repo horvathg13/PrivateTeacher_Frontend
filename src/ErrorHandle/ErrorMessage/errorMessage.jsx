009cad0afae69e48b30336b2df8b2a6bcefa2c1c
horvathg13 <horvathgabor338@gmail.com>
2023-11-22 12:29:01 +0100

    error handle component dev.

--- src/ErrorHandle/ErrorMessage/errorMessage.jsx
+++ src/ErrorHandle/ErrorMessage/errorMessage.jsx
@@ -1,9 +1,12 @@
 import './errorMessage.css';
 import { FaTriangleExclamation } from "react-icons/fa6";
-const ErrorMessage = ({message,messageArray}) => {
+import { IoMdCloseCircle } from "react-icons/io";
+
+const ErrorMessage = ({message,messageArray,closeModal}) => {
     
     return (
         <div className="error-main">
+            <IoMdCloseCircle className='icon close' onClick={()=>{closeModal(true)}}/>
             <div className="error-container flex">
                 <FaTriangleExclamation className='icon'/>
                 {message?<h4>{message}</h4>:null}
---
+++ src/ErrorHandle/errorHandle.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import './errorHandle.css';
+import ErrorMessage from './ErrorMessage/errorMessage';
+        
+const ErrorHandle = ({error}) => {
+
+    const [errorPopup, setErrorPopup]=useState(false);
+    const [validator, setValidator]=useState([]);
+    const [message, setMessage]=useState('');
+
+    useEffect(()=>{
+        if(error?.response?.data?.validatorResponse){
+            setValidator(error.response.data.validatorResponse);
+            setErrorPopup(true);
+        }else if(error?.response?.data?.message){
+            setMessage(error.response.data.message);
+            setErrorPopup(true);
+        }
+    },[error])
+    
+    const close=(data)=>{
+        if(data===true){
+            setValidator([]);
+            setMessage('');
+            setErrorPopup(false);
+        }
+    }
+    return (
+        <div className="errorComponent">
+            { errorPopup ? <ErrorMessage message={message} messageArray={validator} closeModal={close}></ErrorMessage>:null}
+        </div>
+        
+    );
+};
+export default ErrorHandle;
