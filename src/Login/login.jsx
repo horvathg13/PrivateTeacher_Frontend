c076c34761c9f66c7991ba542495c0ca0f9581eb
horvathg13 <horvathgabor338@gmail.com>
2023-11-22 18:16:29 +0100

    login comp developed.

--- src/Login/login.jsx
+++ src/Login/login.jsx
@@ -1,29 +1,70 @@
+import { useState } from 'react';
 import './login.css';
 import { FaArrowCircleRight } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import ServiceClient from '../ServiceClient'
+import ErrorHandle from '../ErrorHandle/errorHandle';
+import Success from '../SuccessPopup/success';
 
 
 const Login = () => {
+    /*Form fields*/
+    const [email, setEmail]=useState('');
+    const [password, setPassword]=useState('');
+
+    /*event handle*/
+    const [errors, setErrors]=useState([]);
+    const [success, setSuccess]=useState(false);
+    const [serverError, setServerError]=useState([]);
+
+    /*btn handler*/
+    const [btndisabled, setBtnDisabled]=useState(false);
+
+    const login=(event)=>{
+        event.preventDefault();
+        setBtnDisabled(true);
+
+        if(email.length>0 && password.length>0){
+            let url='http://127.0.0.1:8000/api/login';
+            let dataPost={};
+            dataPost.email=email;
+            dataPost.psw=password;
+
+            ServiceClient.post(url,dataPost).then((response)=>{
+                if(response.status===200){
+                    setSuccess(true);
+                    localStorage.setItem('token',response.data.data.token);
+                    setBtnDisabled(false);
+                }
+            }).catch((error)=>{
+                setServerError(error);
+                setBtnDisabled(false);
+            })
+        }
+    }
+
     const navigate = useNavigate();
     return (
         <div className="login-container flex">
+            {serverError ? <ErrorHandle error={serverError}/>:null}
+            {success? <Success></Success>:null}
             <div className="left-container">
                 <div className="title">
                     <h1>Log In</h1>
                 </div>
                 <div className="form-container">
-                    <form>
+                    <form onSubmit={login}>
                         <div className="emailPassword-fields grid">
                             <label>Email</label>
-                            <input type="email" require/>
+                            <input type="email" required onChange={(e)=>{setEmail(e.target.value)}}/>
                             
                             <label>Password</label>
-                            <input type="password" require/>
+                            <input type="password" required onChange={(e)=>{setPassword(e.target.value)}}/>
                             
                         </div>
                         
-                        <button className='btn'>Log In <FaArrowCircleRight className='btn-icon'/></button>
-                        
+                        {!btndisabled ? <button type='submit' className='btn'>Log In <FaArrowCircleRight className='btn-icon'/></button>:
+                        <span className='loader'></span>}
                     </form>
                 </div>
                 <div className="mobile-nav-to-register flex">
