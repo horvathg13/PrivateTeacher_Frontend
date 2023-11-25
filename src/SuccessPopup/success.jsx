fa588245fc1d684f5dcffd2f92ac4333c9a5c3aa
horvathg13 <horvathgabor338@gmail.com>
2023-11-25 21:13:43 +0100

    success component rebuild with transitions

--- src/SuccessPopup/success.jsx
+++ src/SuccessPopup/success.jsx
@@ -1,11 +1,23 @@
+import { useRef } from 'react';
 import './success.css';
+import '../transitions.css';
 import { GoCheckCircle  } from 'react-icons/go';
+import {CSSTransition} from 'react-transition-group';
 
-const Success = () => {
+
+const Success = ({success}) => {
+    /*nodRef because of the ReactStrict*/
+    const nodeRef = useRef(null);
     return (
-        <div className="success-container">
-            <GoCheckCircle className="success"/>
-        </div>
+        <CSSTransition nodeRef={nodeRef} in={success} classNames="success" timeout={500} mountOnEnter unmountOnExit>
+            <div className="transition-container" ref={nodeRef}>
+                <GoCheckCircle className='success'/>
+            </div>
+        </CSSTransition>
     );
 };
 export default Success;
+
+/* 'in' control the enter-exit hooks */
+/* 'classNames' : ~ of the hooks */
+/* 'mountOnEnter unmountOnExit' thease are magics whits are control the mounting of the success component*/
