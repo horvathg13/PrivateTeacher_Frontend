da40908b3dc1d54249cfc37d6f65478e2d30950d
horvathg13 <horvathgabor338@gmail.com>
2023-11-28 11:16:04 +0100

    user.jsx created

--- src/Users/user.jsx
+++ src/Users/user.jsx
@@ -1,9 +1,12 @@
-import 'user.css';
+import './user.css';
 import EventHandler from '../EventHandler/eventhandler';
+import Table from '../CommonComponents/Table/table';
         
 const User = () => {
     return (
-        <></>
+        <>
+            <Table />
+        </>
     );
 };
 export default User;
