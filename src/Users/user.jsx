44b3070f0b2185021f71faa1b5b0f9b18ddf4541
horvathg13 <horvathgabor338@gmail.com>
2023-11-29 14:03:01 +0100

    user component modified

--- src/Users/user.jsx
+++ src/Users/user.jsx
@@ -1,11 +1,13 @@
 import './user.css';
 import EventHandler from '../EventHandler/eventhandler';
 import Table from '../CommonComponents/Table/table';
+import SideMenu from '../CommonComponents/Table/SideMenu/sidemenu';
         
 const User = () => {
     return (
         <>
             <Table />
+            <SideMenu />
         </>
     );
 };
