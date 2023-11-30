5b20038ff004ee9959b4cb5ee24fa2c3e8f020cc
horvathg13 <horvathgabor338@gmail.com>
2023-11-30 12:16:03 +0100

    sideMenu modif

--- src/CommonComponents/Table/SideMenu/sidemenu.jsx
+++ src/CommonComponents/Table/SideMenu/sidemenu.jsx
@@ -2,15 +2,16 @@ import './sidemenu.css';
 import {FaBan} from 'react-icons/fa';
 import {RiLockPasswordFill} from 'react-icons/ri';
 import {FaScaleUnbalanced} from 'react-icons/fa6';
+import {RxUpdate} from 'react-icons/rx';
 import menu from './menu.json'        
 
 const SideMenu = () => {
     const getIcon=(iconName)=>{
         switch (iconName) {
-            case 'FaBan':return <FaBan style={{color:'var(--RedColor)'}}/>;
-            case 'FaScaleUnbalanced': return <FaScaleUnbalanced style={{color:'var(--PurpleColor)'}}/>;
-            case 'RiLockPasswordFill': return <RiLockPasswordFill style={{color:'var(--DangerColor)'}}/>;
-           
+            case 'FaBan':return <FaBan className='icon' style={{color:'var(--RedColor)'}}/>;
+            case 'FaScaleUnbalanced': return <FaScaleUnbalanced className='icon' style={{color:'var(--PurpleColor)'}}/>;
+            case 'RiLockPasswordFill': return <RiLockPasswordFill className='icon' style={{color:'var(--DangerColor)'}}/>;
+            case 'RxUpdate': return <RxUpdate className='icon' style={{color:'var(--PrimaryColor)'}}/>;
             default: return null;
         }
     }
@@ -19,8 +20,8 @@ const SideMenu = () => {
             {
                 menu.map((e,i)=>
                     <div className="element-container flex">
-                        <div className="icon">{getIcon(e.icon)}</div>
-                        <div className="name"><h4>{e.name}</h4></div>
+                        <div className="icon-container">{getIcon(e.icon)}</div>
+                        <div className="name-container"><h4>{e.name}</h4></div>
                     </div>
                 )
             }
