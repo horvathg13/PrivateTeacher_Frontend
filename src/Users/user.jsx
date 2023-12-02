1d970d77770a6306abb6f2cdb70cbbb614dce5f5
horvathg13 <horvathgabor338@gmail.com>
2023-12-02 11:08:50 +0100

    users modif

--- src/Users/user.jsx
+++ src/Users/user.jsx
@@ -3,12 +3,45 @@ import EventHandler from '../EventHandler/eventhandler';
 import Table from '../CommonComponents/Table/table';
 import SideMenu from '../CommonComponents/SideMenu/sidemenu';
 import ComponentTitle from '../CommonComponents/ComponentTitle/componentTitle';
+import ServiceClient from '../ServiceClient';
+import { useEffect, useLayoutEffect, useState } from 'react';
         
 const User = () => {
+    const [users, setUsers]=useState({});
+    const [loader, setLoader]=useState(false);
+
+    /*event handle*/
+    const [errors, setErrors]=useState([]);
+    const [success, setSuccess]=useState(false);
+    const [serverError, setServerError]=useState([]);
+
+
+    useLayoutEffect(()=>{
+        setLoader(true);
+       let url='http://127.0.0.1:8000/api/getUsers';
+       ServiceClient.post(url).then((response)=>{
+        if(response.status===200){
+            setLoader(false);
+            setUsers(response.data);
+            console.log(response.data)
+        }
+       }).catch((error)=>{
+        setServerError(error);
+        setLoader(false);
+       })
+    },[])
+
+    useEffect(()=>{console.log(users)},[users])
     return (
         <>
+        <EventHandler 
+        success={success} 
+        errors={errors} 
+        serverError={serverError} 
+        closeErrorMessage={(data)=>{if(data===true){setErrors([])}}}/>
         <ComponentTitle />
-        <Table /> 
+        <Table datas={users ? users :null}
+        loader={loader}/> 
         <SideMenu />
         </>
             
