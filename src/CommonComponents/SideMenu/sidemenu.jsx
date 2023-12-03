fd0421d1509fc862ee32d24c64ca3ac89bb3f5cd
horvathg13 <horvathgabor338@gmail.com>
2023-12-03 20:14:42 +0100

    sidemenu modif -- new conception

--- src/CommonComponents/SideMenu/sidemenu.jsx
+++ src/CommonComponents/SideMenu/sidemenu.jsx
@@ -1,39 +1,39 @@
 import './sidemenu.css';
-import {FaBan} from 'react-icons/fa';
+import {FaBan, FaUsers ,FaUserPlus} from 'react-icons/fa';
 import {RiLockPasswordFill} from 'react-icons/ri';
 import {FaScaleUnbalanced} from 'react-icons/fa6';
 import {RxUpdate} from 'react-icons/rx';
 import menu from './menu.json'        
 import { CSSTransition } from 'react-transition-group';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import '../../transitions.css'
 
 const SideMenu = ({active}) => {
     const getIcon=(iconName)=>{
         switch (iconName) {
             case 'FaBan':return <FaBan className='menu-icon icon'/>;
-            case 'FaScaleUnbalanced': return <FaScaleUnbalanced className='menu-icon icon' />;
-            case 'RiLockPasswordFill': return <RiLockPasswordFill className='menu-icon icon'/>;
-            case 'RxUpdate': return <RxUpdate className='menu-icon icon'/>;
-
+            //case 'FaScaleUnbalanced': return <FaScaleUnbalanced className='menu-icon icon' />;
+            //case 'RiLockPasswordFill': return <RiLockPasswordFill className='menu-icon icon'/>;
+            case 'FaUsers': return <FaUsers className='menu-icon icon'/>;
+            case 'FaUserPlus':return <FaUserPlus className='menu-icon icon'/>;
             default: return null;
         }
     }
+    const [selectedMenu,setSelectedMenu]=useState(1);
+        
 
     const nodeRef = useRef(null);
     return (
-        <CSSTransition nodeRef={nodeRef} in={active} classNames="sideMenu" timeout={500} mountOnEnter unmountOnExit>
-            <div className="side-main-conainer" ref={nodeRef}>
-                {
-                    menu.map((e,i)=>
-                        <div className="element-container flex">
-                            <div className="icon-container">{getIcon(e.icon)}</div>
-                            <div className="name-container"><h4>{e.name}</h4></div>
-                        </div>
-                    )
-                }
-            </div>
-        </CSSTransition>
+        <div className="side-main-conainer" ref={nodeRef}>
+            {
+                menu.map((e,i)=>
+                    <div className="element-container flex" key={i}>
+                        <div className={selectedMenu === e.id ? "icon-container active" : "icon-container"} onClick={()=>setSelectedMenu(e.id, console.log(e))}>{getIcon(e.icon)}</div>
+                        <div className="name-container"><h4>{e.name}</h4></div>
+                    </div>
+                )
+            }
+        </div>
     );
 };
 export default SideMenu;
