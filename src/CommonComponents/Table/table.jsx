02b0690db8d1528e2e1dd1baadb2618bb9c3baa1
horvathg13 <horvathgabor338@gmail.com>
2023-12-03 20:14:55 +0100

    table comp modif

--- src/CommonComponents/Table/table.jsx
+++ src/CommonComponents/Table/table.jsx
@@ -9,71 +9,65 @@ import { useEffect, useState } from 'react';
 const Table = ({datas, loader, page, perPage,selectedRow}) => {
     /* data: */
 
-    const [active, setActive]=useState();
+    const [activeRow, setActiveRow]=useState();
    
     /* methods:*/
-    const setRowActive=(e)=>{
-        if(e.id){
-            return "active"
-        }
-    }
-    
+
     useEffect(()=>{console.log(datas, loader)},[datas])
     return (
         <>
         {!loader ?
+        <><button onClick={()=>setActiveRow(false)}>Delete Selection</button>
         <div className="table-main-container">
-            
-            
+
+
             <div className="table-action-menu flex">
-                <button className='btn table-btn'><FaFilter className='table-menu-icon'/>Filter</button>
-                <button className='btn table-btn'><FaFilter className='table-menu-icon'/>Filter</button>
-                <button className='btn table-btn'><FaFilter className='table-menu-icon'/>Filter</button>
+                <button className='btn table-btn'><FaFilter className='table-menu-icon' />Filter</button>
+                <button className='btn table-btn'><FaFilter className='table-menu-icon' />Filter</button>
+                <button className='btn table-btn'><FaFilter className='table-menu-icon' />Filter</button>
             </div>
             <table>
                 <thead>
-                    
-                    
-                    <tr> 
-                        {
-                        datas.header? Object.keys(datas.header).map((e,i)=>(
-                           
-                            <th key={i}>{e} {datas.header[e]===true ? <><FaSortAmountUp className='table-icon' /><FaSortAmountDown className='table-icon' /></>:null}</th>
 
-                        )):null
-                        }
-                       
+
+                    <tr>
+                        {datas.header ? Object.keys(datas.header).map((e, i) => (
+
+                            <th key={i}>{e} {datas.header[e] === true ? <><FaSortAmountUp className='table-icon' /><FaSortAmountDown className='table-icon' /></> : null}</th>
+
+                        )) : null}
+
                     </tr>
-                   
-                    
+
+
                 </thead>
                 <tbody>
-                    { datas.data?.map((e)=>(
-                        <tr key={e.id} onClick={()=>[selectedRow(e), setRowActive(e)]} className={''}>
+                    {datas.data?.map((e) => (
+                        <tr key={e.id} onClick={() => [selectedRow(e), setActiveRow(e)]} className={activeRow === e ? 'active' : ''}>
                             <td>{e.id}</td>
                             <td>{e.firstname}</td>
                             <td>{e.lastname}</td>
                             <td>{e.email}</td>
                             <td>{e.status}</td>
                         </tr>
-                    ))
-                    }
-                    
+                    ))}
+
                 </tbody>
             </table>
 
             <div className="pagination-continer flex">
                 <div className="show-container flex">
-                    Show/page<input type='number' onKeyDown={(e)=>{if(e.key === 'Enter'){return perPage(e.target.value)}}}/>
-                    
+                    Show/page<input type='number' onKeyDown={(e) => { if (e.key === 'Enter') { return perPage(e.target.value); } } } />
+
                 </div>
                 <div className="pagination flex">
-                    <MdFirstPage className='table-icon' onClick={()=>page('first')} /><GrFormPrevious  className='table-icon' onClick={()=>page('prev')}/>{datas?.pagination?.currentPageNumber} / {datas?.pagination?.lastPageNumber}{datas?.pagination?.hasMorePages ? <><MdNavigateNext className='table-icon' onClick={()=>page('next')}/><MdLastPage className='table-icon' onClick={()=>page('last')}/></>:null}
+                    <MdFirstPage className='table-icon' onClick={() => page('first')} /><GrFormPrevious className='table-icon' onClick={() => page('prev')} />{datas?.pagination?.currentPageNumber} / {datas?.pagination?.lastPageNumber}{datas?.pagination?.hasMorePages ? <><MdNavigateNext className='table-icon' onClick={() => page('next')} /><MdLastPage className='table-icon' onClick={() => page('last')} /></> : null}
                 </div>
-            </div> 
+            </div>
 
-            
-        </div>: <span className='loader'></span>}</>
+
+        </div></>: <span className='loader table'></span>}
+        </>
            
     );
 };
