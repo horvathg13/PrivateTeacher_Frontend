3359398dffefa14317e982ead6c78b71149aa869
horvathg13 <horvathgabor338@gmail.com>
2023-12-06 22:59:36 +0100

    are you sure modal modif

--- src/CommonComponents/AreYouSure/areyousure.jsx
+++ src/CommonComponents/AreYouSure/areyousure.jsx
@@ -3,14 +3,14 @@ import './areyousure.css';
 import { CSSTransition } from 'react-transition-group';
 import '../../transitions.css'
         
-const AreYouSure = ({name, answer, transitionProp}) => {
+const AreYouSure = ({label, name, answer, transitionProp}) => {
     const nodeRef=useRef(null)
     return (
         <CSSTransition nodeRef={nodeRef} in={transitionProp} classNames="fade" timeout={500} mountOnEnter unmountOnExit>
             <div className="are-you-sure-bcg" ref={nodeRef}>
                 <div className="are-yousure-main">
                     <div className="title">
-                        <h1>Are you Sure?</h1>
+                        <h1>{label ? label : 'Are You Sure?'}</h1>
                     </div>
                     <div className="answers flex">
                         <button onClick={()=>{answer(name)}} >Yes</button>
