6048342a8b1ce8f139b7b9acdbad03fda9a13bd5
horvathg13 <horvathgabor338@gmail.com>
2023-12-09 00:43:42 +0100

    new route handling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import * as React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, RouterProvider, Outlet, useParams} from "react-router-dom";
 import Register from "./Register/register"
 import Header from './Header/header';
 import Login from './Login/login';
@@ -10,30 +10,51 @@ import userRoles from './Users/userRolesHandler/userRoles';
 import Protected from './ProtectedRoutes';
 import { UserContextProvider } from './Context/UserContext';
 import SelectedUserComponent from './Users/selectedUserHandler/selectedUser';
+import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import ServiceClient from './ServiceClient';
+import { userDataLoader } from './dataLoader';
 
 function App() {
+
+  const Root = ()=>{
+    return(
+      <>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+      
+      </>
+    )
+  }
+  const router =createBrowserRouter(
+    createRoutesFromElements(
+      
+      <Route path='/' element={<Root/>}>
+          
+        <Route index element={<Login />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route element={<Protected/>}>
+          <Route path="/home" element={<Home />}/>
+          <Route path="/users" element={<Users />}/>
+          <Route path="/users/:userId" loader={({params})=>{ return userDataLoader(params) }} element={<SelectedUserComponent />}/>
+          <Route path="/users/:userId/roles" element={<userRoles />}/>
+          
+        </Route>
+          
+      </Route>
+      
+    )
+  )
+    
+  
+
   return (
     <>
     <UserContextProvider>
-      <Router>
-        <Header />
-
-        <Routes>
-          
-          <Route path="/" element={<Login />}/>
-          <Route path="/register" element={<Register />}/>
-          <Route element={<Protected/>}>
-            <Route path="/home" element={<Home />}/>
-            <Route path="/users" element={<Users />}>
-              <Route path=":userId" element={<SelectedUserComponent />}/>
-              <Route path=":userId/roles" element={<userRoles />}/>
-            </Route>
-            
-          </Route>
-          
-        </Routes>
-
-      </Router>
+      <RouterProvider router={router}/>
     </UserContextProvider>
     </>
   );
