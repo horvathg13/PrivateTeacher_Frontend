c11e54d70215d6f63d89ea1a6fa7e445aa155c6f
horvathg13 <horvathgabor338@gmail.com>
2023-12-09 20:42:34 +0100

    userRoles modif

--- src/Users/userRolesHandler/userRoles.jsx
+++ src/Users/userRolesHandler/userRoles.jsx
@@ -1,8 +1,56 @@
+import { useState } from 'react';
+import ComponentTitle from '../../CommonComponents/ComponentTitle/componentTitle';
+import SideMenu from '../../CommonComponents/SideMenu/sidemenu';
+import EventHandler from '../../EventHandler/eventhandler';
 import './userRoles.css';
+import { useNavigate, useParams } from 'react-router-dom';
         
-const userRoles = () => {
+const UserRoles = () => {
+    /*event handle*/
+    const [errors, setErrors]=useState([]);
+    const [success, setSuccess]=useState(false);
+    const [serverError, setServerError]=useState([]);
+    const navigate = useNavigate();
+
+    /*btn handle*/
+    const [btndisabled, setBtnDisabled]=useState(false);
+    const [loader, setLoader]=useState(false);
+    /*DataSave*/
+    const [dataSave, setDataSave]=useState();
+    let { userId }=useParams();
+
+    /*TabMenu */
+    const tabData=[
+        {
+            "id":"1",
+            "name":"Info",
+            "url":`/users/${userId}`
+        },
+        {
+            "id":"2",
+            "name":"Roles",
+            "url":`/users/${userId}/roles`
+        },
+        {
+            "id":"3",
+            "name":"Logs",
+            "url":`/users/${userId}/logs`
+        }
+        
+    ]
     return (
-        <></>
+        <div className="userRoles-main-container">
+            <EventHandler 
+                success={success} 
+                errors={errors} 
+                serverError={serverError} 
+                closeErrorMessage={(data)=>{if(data===true){setErrors([])}}}/>
+            <ComponentTitle />
+            <SideMenu/>
+            <div className="userRoles-container">
+                
+            </div>
+        </div>
     );
 };
-export default userRoles;
+export default UserRoles;
