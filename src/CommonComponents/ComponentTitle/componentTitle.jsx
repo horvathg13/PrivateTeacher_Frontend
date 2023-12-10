d3bdbc0f322166d4016fd67f35fd77c7fd515ca0
horvathg13 <horvathgabor338@gmail.com>
2023-12-10 23:19:09 +0100

    componentTitle modif

--- src/CommonComponents/ComponentTitle/componentTitle.jsx
+++ src/CommonComponents/ComponentTitle/componentTitle.jsx
@@ -1,12 +1,24 @@
+import { IoIosArrowForward } from "react-icons/io";
 import "./componentTitle.css";
+import { NavLink } from "react-router-dom";
         
-const ComponentTitle = () => {
+const ComponentTitle = ({breadcumbs}) => {
+    const getIcon=(iconName)=>{
+        switch (iconName) {
+            case 'IoIosArrowForward':return <IoIosArrowForward className='breadicon'/>;
+            default: return null;
+        }
+    }
     return (
         <div className="component-title-container flex">
             <div className="title">
                 <h1>Users</h1>
             </div>
-            <div className="breadcumbs"><h4>Home {'>'} Hello {'>'} Hello</h4></div>
+            <div className="breadcumbs flex">
+                {breadcumbs.map((e)=>
+                <><h4 key={e.id}><NavLink to={e.url}>{e.name}</NavLink></h4>{getIcon(e.icon)}</>
+                )}
+            </div>
             <div className="nofitications"><h4>New Message Arrived</h4></div>
         </div>
     );
