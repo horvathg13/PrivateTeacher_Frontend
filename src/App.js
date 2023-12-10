b9c4f2d43cabacb673a6d36df623ee0e8f4492e9
horvathg13 <horvathgabor338@gmail.com>
2023-12-10 15:45:52 +0100

    app js modified: new route concept dev

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Register from "./Register/register"
 import Header from './Header/header';
 import Login from './Login/login';
 import Home from './Home/home';
-import Users from './Users/user';
-import UserRoles from './Users/userRolesHandler/userRoles';
+import UserList from './Users/userList/userList';
+import UserRoles from './Users/user/userRolesHandler/userRoles';
+import UserLog from './Users/user/userLogs/userLog';
 import Protected from './ProtectedRoutes';
 import { UserContextProvider } from './Context/UserContext';
-import UserDetailsComponent from './Users/userDetails/userDetails';
+import UserDetailsComponent from './Users/user/userDetails/userDetails';
 import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import ServiceClient from './ServiceClient';
 import { userDataLoader } from './dataLoader';
+import User from './Users/user/user';
 
 function App() {
 
@@ -29,26 +31,52 @@ function App() {
       </>
     )
   }
-  const router =createBrowserRouter(
-    createRoutesFromElements(
-      
-      <Route path='/' element={<Root/>}>
-          
-        <Route index element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route element={<Protected/>}>
-          <Route path="/home" element={<Home />}/>
-          <Route path="/users" element={<Users />}/>
-          <Route path="/users/:userId" loader={({params})=>{ return userDataLoader(params) }} element={<UserDetailsComponent />}/>
-          <Route path="/users/:userId/roles" element={<UserRoles />}/>
-          
-        </Route>
-          
-      </Route>
-      
-    )
-  )
-    
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Login />,
+      },
+      {
+        path:"register",
+        element:<Register />
+      },
+      {
+        path:"home",
+        element:<Protected><Home /></Protected>
+      },
+      {
+        path:"users",
+        element:<Protected><UserList /></Protected>,
+      },
+      {
+        path:"users/:userId",
+        element:<Protected><User /></Protected>,
+        loader:({params})=>{ return userDataLoader(params) },
+        children:[
+          {
+            path:"",
+            element:<UserDetailsComponent />,
+            
+          },
+          {
+            path:"roles",
+            element:<UserRoles />
+          },
+          {
+            path:"logs",
+            element:<UserLog />
+          }
+        ]
+      }
+    ],
+  },
+]);
+  
   
 
   return (
