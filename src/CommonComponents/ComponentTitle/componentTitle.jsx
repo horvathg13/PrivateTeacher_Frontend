40263317f93c458fbc2da317ac45066a4f20c3af
horvathg13 <horvathgabor338@gmail.com>
2023-12-10 23:49:35 +0100

    compoent Title modif

--- src/CommonComponents/ComponentTitle/componentTitle.jsx
+++ src/CommonComponents/ComponentTitle/componentTitle.jsx
@@ -16,7 +16,7 @@ const ComponentTitle = ({breadcumbs}) => {
             </div>
             <div className="breadcumbs flex">
                 {breadcumbs.map((e)=>
-                <><h4 key={e.id}><NavLink to={e.url}>{e.name}</NavLink></h4>{getIcon(e.icon)}</>
+                <><h4 key={e.id}><NavLink to={e.url} end={e.end}>{e.name}</NavLink></h4>{getIcon(e.icon)}</>
                 )}
             </div>
             <div className="nofitications"><h4>New Message Arrived</h4></div>
