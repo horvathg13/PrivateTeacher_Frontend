88fca9a36b22edc6859496006ce8c0eab1a905d2
horvathg13 <horvathgabor338@gmail.com>
2023-12-10 22:06:49 +0100

    new loader definied

--- src/dataLoader.js
+++ src/dataLoader.js
@@ -4,3 +4,11 @@ export const userDataLoader=(params)=>{
     return ServiceClient.get(`http://127.0.0.1:8000/api/selectedUserData/${params.userId}`)
 
 }
+
+export const generatedUserLoader=(params)=>{
+    return ServiceClient.get(`http://127.0.0.1:8000/api/password-reset/${params.token}`).then((response)=>{
+        if(response.status===200){
+            return response.data
+        }
+    })
+}
