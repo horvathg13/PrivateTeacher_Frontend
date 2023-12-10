56871f3624f82f1027ee7df677a2cc82a8670dfc
horvathg13 <horvathgabor338@gmail.com>
2023-12-10 22:08:16 +0100

    users modif

---
+++ src/Users/userHome/users.jsx
@@ -0,0 +1,46 @@
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import ComponentTitle from "../../CommonComponents/ComponentTitle/componentTitle";
+import SideMenu from "../../CommonComponents/SideMenu/sidemenu";
+import TabMenu from "../../CommonComponents/TabMenu/tabMenu";
+import { PiUserListFill  } from "react-icons/pi";
+import { FaUserPlus } from "react-icons/fa6";
+import "./users.css";
+        
+const Users = () => {
+    const navigate= useNavigate();
+    const tabData=[
+        {
+            "id":"1",
+            "name":"User List",
+            "url":'/users/list',
+        },
+        {
+            "id":"2",
+            "name":"Create",
+            "url":'/users/create'
+        },
+        
+    ]
+    return (
+        <div className="user-main-container">
+            <ComponentTitle />
+            <SideMenu/> 
+            <div className="user-main">
+                { window.location.pathname === '/users' ? 
+                <div className="button-main-container flex">
+                    <div className="button-container">
+                        <Link to={"/users/list"}><div className="icon-container"><PiUserListFill  className="icon"/></div></Link>
+                        <div className="icon-text"><h3>List</h3></div>
+                    </div>
+                    <div className="button-container">
+                        <Link to={"/users/create"}><div className="icon-container" ><FaUserPlus className="icon"/></div></Link>
+                        <div className="icon-text"><h3>Create</h3></div>
+                    </div>
+                   
+                </div>:<TabMenu menu={tabData}/>}
+                <Outlet/>
+            </div>
+        </div>
+    );
+};
+export default Users;
