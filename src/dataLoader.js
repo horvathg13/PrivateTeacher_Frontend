f51b689f5c00a45f59cdec20ca4c38a9500ddc1f
horvathg13 <horvathgabor338@gmail.com>
2023-12-10 23:18:27 +0100

    dataLoader modif

--- src/dataLoader.js
+++ src/dataLoader.js
@@ -1,7 +1,11 @@
 import ServiceClient from "./ServiceClient"
 
 export const userDataLoader=(params)=>{
-    return ServiceClient.get(`http://127.0.0.1:8000/api/selectedUserData/${params.userId}`)
+    return ServiceClient.get(`http://127.0.0.1:8000/api/selectedUserData/${params.userId}`).then((response)=>{
+        if(response.status===200){
+            return response.data
+        }
+    })
 
 }
 
