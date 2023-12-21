4f4d85f3711e9cd96fec85837ca3e85f8c1a521c
horvathg13 <horvathgabor338@gmail.com>
2023-12-21 20:48:06 +0100

    schoolYear List modif

--- src/Schools/School/SchoolYearList/schoolYearList.jsx
+++ src/Schools/School/SchoolYearList/schoolYearList.jsx
@@ -1,5 +1,6 @@
 import { MdEdit } from "react-icons/md";
 import "./schoolYearList.css";
+import "../../../CommonComponents/Table/table.css"
 import { FaCirclePlus } from "react-icons/fa6";
 import Table from "../../../CommonComponents/Table/table";
 import { Outlet, useLoaderData, useNavigate, useParams } from "react-router-dom";
@@ -7,6 +8,8 @@ import SideMenu from "../../../CommonComponents/SideMenu/sidemenu";
 import { useContext, useLayoutEffect, useState } from "react";
 import EventHandler from "../../../EventHandler/eventhandler";
 import ServiceClient from "../../../ServiceClient";
+import { FaPlus } from "react-icons/fa";
+import AddSchoolYear from "./AddSchoolYear/addSchoolYear";
         
 const SchoolYearList = () => {
     /*Datas */
@@ -16,6 +19,8 @@ const SchoolYearList = () => {
     const [selectedRow, setSelectedRow]=useState();
     let { schoolId }=useParams();
     const [schoolYears, setSchoolYears]=useState();
+    const [transitionProp, setTransitionProp]=useState(false);
+    const [btndisabled, setBtnDisabled]=useState(false);
 
     /*event handle*/
     const [errors, setErrors]=useState([]);
@@ -24,51 +29,109 @@ const SchoolYearList = () => {
 
     /*Loader */
     const [loader, setLoader]=useState(true);
+    const [formLoader, setFormLoader]=useState(false);
 
     /*Navigation */
     const navigation=useNavigate();
 
     /*Methods */
-    const pageCounter=(data)=>{
-        switch (data){
-            case 'next': return setCounter(counter+1);
-            case 'prev': if(counter >1){return setCounter(counter-1)}else{return null};
-            case 'last': return setCounter(lastPage);
-            case 'first':return setCounter(1);
-            default: return counter;
-        }
-    }
-
-    useLayoutEffect(()=>{
-        let url=`http://127.0.0.1:8000/api/school-year-list/${schoolId}?perPage=${perPage}&page=${counter}`;
+    const getSchoolYears=()=>{
+        let url=`http://127.0.0.1:8000/api/school-year-list/${schoolId}`;
         ServiceClient.post(url).then((response)=>{
             if(response.status===200){
                 setLoader(false);
                 setSchoolYears(response.data);
-                setLastPage(response.data.pagination.lastPageNumber)
-                console.log(response.data)
+                //console.log(response.data)
             }
         }).catch((error)=>{
             setServerError(error);
             setLoader(false);
         })
-       
-    },[counter, perPage])
+    }
+
+    useLayoutEffect(()=>{
+        getSchoolYears();
+    },[])
+
+    const CreateSchoolYear=(dataForm)=>{
+        setFormLoader(true);
+        setBtnDisabled(true);
+        console.log(dataForm)
+        let url="http://127.0.0.1:8000/api/createSchoolYear";
+        ServiceClient.post(url, dataForm).then((response)=>{
+            if(response.status===200){
+                setFormLoader(false);
+                setBtnDisabled(false);
+                setSuccess(true);
+                setTimeout(()=>{
+                    setSuccess(false);
+                },2000)
+                getSchoolYears();
+            }
+        }).catch((error)=>{
+            setServerError(error);
+            setLoader(false);
+            setBtnDisabled(false)
+        })
+        
+
+    }
+
     return (
         <>
         <EventHandler 
             success={success} 
             errors={errors} 
             serverError={serverError} 
-            closeErrorMessage={(data)=>{if(data===true){setErrors([])}}}/>
+            closeErrorMessage={(data)=>{if(data===true){setErrors([])}}}
+        />
+        <AddSchoolYear 
+            transitionProp={transitionProp}
+            closeModal={(data)=>{if(data===true){setTransitionProp(!transitionProp); setFormLoader(false); setBtnDisabled(false)}}}
+            emitData={(dataForm)=>{CreateSchoolYear(dataForm)}}
+            loader={formLoader}
+            btndisabled={btndisabled}
+        />  
         <div className="content-main-container">
+            
             <div className="table-main-container">
-                <Table 
+                {!loader ? 
+                <table>
+                    <thead>
+                        <tr>
+                            {schoolYears.header ? Object.keys(schoolYears.header).map((e, i) => (
+
+                                <th key={i}>{e}</th>
+                                
+
+                            )) : null}
+
+                        </tr>
+
+                    </thead>
+                    <tbody>
+                        { schoolYears.data?.map((e) => (
+                            <tr key={e.id} onClick={() => selectedRow(e)}>
+                            { Object.values(e).map((j=>
+                                <td>{j}</td>
+                            ))}
+                            </tr>
+                        ))}
+                        {schoolYears.data.length===0 ?
+                        <tr>
+                             <td colSpan={5} className="no-school" >No registered school year in this school.</td>
+                        </tr>:null}
+                        <tr>
+                            <td><FaPlus className='table-action-icon' onClick={()=>setTransitionProp(true)}/></td>
+                        </tr>
+                    </tbody>
+                </table> : <span className='loader table'></span>}
+                {/*<Table 
                 datas={schoolYears ? schoolYears : null}
                 loader={loader}
                 page={pageCounter}
                 perPage={setPerPage}
-                selectedRow={(e)=>[setSelectedRow(e)]}/>
+                selectedRow={(e)=>[setSelectedRow(e)]}/>*/}
             </div>
             <Outlet/>
         </div>
