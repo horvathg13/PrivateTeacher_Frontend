4f522d501781fd9cd4bff1a121a66c4d2f6eca57
horvathg13 <horvathgabor338@gmail.com>
2024-02-06 11:05:00 +0100

    tabMenu component is modif

--- src/CommonComponents/TabMenu/tabMenu.jsx
+++ src/CommonComponents/TabMenu/tabMenu.jsx
@@ -1,15 +1,22 @@
 import { NavLink, useNavigate } from 'react-router-dom';
 import './tabMenu.css';
+import { useContext } from 'react';
+import { TabMenuContext } from '../../Context/UserContext';
         
 const TabMenu = ({menu}) => {
     const navigate=useNavigate();
+    const {menuItem,setMenuItems} = useContext(TabMenuContext);
     return (
         
         <div className="tabMenu-container flex">
-        { menu.map((e)=>
+        { /*menu.map((e)=>
+           <NavLink to={e.url} end={e.end}><button className={'btn action flex'}>{e.name}</button></NavLink>
+        )*/}
+        { menuItem?.map((e)=>
            <NavLink to={e.url} end={e.end}><button className={'btn action flex'}>{e.name}</button></NavLink>
         )}
         </div>
+        
         
     );
 };
