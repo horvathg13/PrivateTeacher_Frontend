2e56eb14ae1068316b18062fc56973e69f39b255
horvathg13 <horvathgabor338@gmail.com>
2024-03-19 23:34:16 +0100

    labelSelect modified

---
+++ src/CommonComponents/Label/labelSelect.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import LabelPopup from "./labelPopup";
+import { FaPlus } from "react-icons/fa";
+
+        
+const LabelSelector = ({transition, labelEmit, getLabels}) => {
+    
+    const [labels, setLabels]=useState();
+    const [labelTransition, setLabelTransition]=useState();
+
+    useEffect(()=>{
+        if(transition){
+            setLabelTransition(transition);
+        }
+    },[transition])
+
+    useEffect(()=>{
+        labelEmit(labels);
+    },[labels]);
+    useEffect(()=>{
+        setLabels(getLabels);
+    },[]);
+    return (
+
+
+        <div className="selector-main">
+            {labelTransition ? 
+            <LabelPopup 
+            labelTransition={labelTransition} 
+            closeModal={(data)=>{if(data===true){setLabelTransition(false)}}} 
+            selection={(data)=>setLabels(data)}
+            selected={labels}/> : <input className="selector-input" type="text" value={labels?.map(e=>e.label)} readOnly/>}
+            <FaPlus className="selector-icon" onClick={()=>setLabelTransition(true)}/>
+        </div>
+    );
+};
+export default LabelSelector;
