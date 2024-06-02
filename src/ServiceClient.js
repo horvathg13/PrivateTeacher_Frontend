b5a94f009e393cfa6d587fd728c1dd878a333e5d
hgabor13 <horvathgabor338@gmail.com>
2024-06-02 21:50:53 +0200

    new ServiceCalls added

--- src/ServiceClient.js
+++ src/ServiceClient.js
@@ -57,17 +57,26 @@ class ServiceClient {
             return response.data
         })
     }
-    static schoolUpdate(schoolId,name,city,zip,number){
-        return this.post("http://127.0.0.1:8000/api/schoolUpdate", {id: schoolId, name:name, city:city, zip:zip, number:number}).then((response)=>{
+    static schoolUpdate(schoolId,name,city,zip,street, number){
+        return this.post("http://127.0.0.1:8000/api/schoolUpdate", {id: schoolId, name:name, city:city, zip:zip, street:street, number:number}).then((response)=>{
             return response.data
         })
     }
     static getSchoolYears(schoolId){
-        this.post(`http://127.0.0.1:8000/api/school-year-list/${schoolId}`).then((response)=> {
+        return this.post(`http://127.0.0.1:8000/api/school-year-list/${schoolId}`).then((response)=> {
             return response.data;
         })
     }
-
+    static getSchools(perPage,counter){
+        return this.post(`http://127.0.0.1:8000/api/schools-list?perPage=${perPage}&page=${counter}`).then((response)=>{
+            return response.data
+        })
+    }
+    static schoolCreate(name,country, zip, city, street, number){
+        return this.post("http://127.0.0.1:8000/api/schoolCreate", {name:name, country:country, city:city, zip:zip, street:street, number:number}).then((response)=>{
+            return response.data
+        })
+    }
 }
 
 export default ServiceClient
