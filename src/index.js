3abb8499133ac6c63079f7b25a31dbb740eb3cfd
hgabor13 <horvathgabor338@gmail.com>
2024-06-10 21:25:39 +0200

    i18next added, inicialized

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
+import {initReactI18next} from "react-i18next";
+import backend from "i18next-http-backend";
+import i18next from "i18next";
+
+const lng =localStorage.getItem('i18nextLng');
+export const languageTransform=(lng)=>{
+    switch (lng){
+        case 'GB':return 'en-GB'
+        case 'HU':return 'hu-HU'
+        default: return 'hu-HU'
+    }
+}
+const ns = "translation"
+i18next
+    .use(I18nextBrowserLanguageDetector)
+    .use(initReactI18next)
+    .use(backend)
+    .init({
+        debug:false,
+        lng:lng,
+        fallbackLng:'hu-HU',
+        ns:"translation",
+        backend: {
+            loadPath:'/locales/{{lng}}/translation.json',
+        },
+        initImmediate:true,
+    })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Suspense fallback={"Loading..."}>
     <App />
-  </React.StrictMode>
+  </Suspense>
 );
 
 // If you want to start measuring performance in your app, pass a function
