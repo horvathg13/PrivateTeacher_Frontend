8e45acf2df10a5f234671003fe79c67ebb7ce96a
hgabor13 <horvathgabor338@gmail.com>
2024-10-30 17:57:24 +0100

    new components

---
+++ src/Child/childCourses.jsx
@@ -0,0 +1,78 @@
+import React, {useEffect, useState} from 'react';
+import {useTranslation} from "react-i18next";
+import {useLoaderData, useNavigate} from "react-router-dom";
+import EventHandler from "../EventHandler/eventhandler";
+
+const ChildCourses = () => {
+    /*Translation*/
+    const {t}=useTranslation();
+
+    /*Data*/
+    const loaderData=useLoaderData();
+    const [loader, setLoader]=useState(true);
+    const [selectedRow, setSelectedRow]=useState();
+
+    /*Navigation*/
+    const navigate = useNavigate();
+
+    /*Event handle*/
+    const [errors, setErrors]=useState([]);
+    const [success, setSuccess]=useState(false);
+    const [serverError, setServerError]=useState([]);
+
+    /*Methods: */
+
+    useEffect(()=>{
+        if(loaderData){
+            setLoader(false);
+        }
+    },[selectedRow])
+    return (
+        <div>
+
+            <EventHandler
+                success={success}
+                errors={errors}
+                serverError={serverError}
+                closeErrorMessage={(data) => {
+                    if (data === true) {
+                        setErrors([])
+                    }
+                }}
+            />
+            <div className="table-main-container">
+                {!loader ?
+                    <table>
+                        <thead>
+                        <tr>
+                            {loaderData.header ? loaderData.header.map((e, i) => (
+
+                                <th key={i}>{e}</th>
+
+
+                            )) : null}
+                        </tr>
+
+                        </thead>
+                        <tbody>
+                        {loaderData.data?.map((e,i) => (
+                            <tr key={i} onClick={() => navigate(`/course/${e.teacher_course_id}`)}>
+                                <td>{e.id}</td>
+                                <td>{e.name}</td>
+                                <td>{e.teacher}</td>
+                                <td>{e.status}</td>
+                            </tr>
+                        ))}
+                        {loaderData.data.length === 0 ?
+                            <tr>
+                                <td colSpan={5} className="no-school">{t('empty-table')}</td>
+                            </tr> : null}
+                        </tbody>
+                    </table> : <span className='loader table'></span>}
+            </div>
+        </div>
+    );
+
+};
+
+export default ChildCourses;
---
+++ src/Requests/requestDetails.jsx
@@ -0,0 +1,207 @@
+import React, {useContext, useEffect, useState} from 'react';
+import EventHandler from "../EventHandler/eventhandler";
+import {useTranslation} from "react-i18next";
+import {FaArrowCircleRight, FaCheck} from "react-icons/fa";
+import {useLoaderData, useNavigate, useParams} from "react-router-dom";
+import {IoMdClose} from "react-icons/io";
+import ServiceClient from "../ServiceClient";
+import AreYouSure from "../CommonComponents/AreYouSure/areyousure";
+import {UserContext} from "../Context/UserContext";
+
+const RequestDetails = () => {
+    /*Translation*/
+    const {t}=useTranslation("translation",{keyPrefix:"requestDetails"});
+
+    /*Data*/
+    const requestDetails=useLoaderData();
+    const requestId=useParams();
+    const {roles}=useContext(UserContext);
+    const [isTeacher, setIsTeacher]=useState();
+
+    /*Event handle*/
+    const [errors, setErrors]=useState([]);
+    const [success, setSuccess]=useState(false);
+    const [serverError, setServerError]=useState([]);
+    /*Loader */
+    const [acceptLoader, setAcceptLoader]=useState(false);
+    const [rejectLoader, setRejectLoader]=useState(false);
+
+    /*Navigation */
+    const navigation=useNavigate();
+
+    /*button control */
+    const [btndisabled, setBtnDisabled]=useState(false);
+    const [readOnly, setReadOnly]=useState(false);
+
+    /*AreYouSure*/
+    const [areYouSureTransitionProp, setAreYouSureTransitionProp]=useState();
+    const [areYouSureName, setAreYouSureName]=useState('');
+
+    /*Methods*/
+
+    useEffect(() => {
+        if(roles.some(i=>i==='Teacher')){
+            setIsTeacher(true);
+        };
+    }, [roles]);
+    const functionControl=(name)=>{
+        if(name==='accept'){
+            accept();
+        }
+        if(name==='reject'){
+            reject();
+        }
+    }
+    const accept=()=>{
+        setAcceptLoader(true)
+        setBtnDisabled(true)
+
+        ServiceClient.acceptCourseRequest(requestId).then((success)=>{
+            setSuccess(true);
+            setTimeout(()=>{
+                setSuccess(false);
+            },2000)
+            navigation('/requests');
+        }).catch((error)=>{
+            setServerError(error);
+            setBtnDisabled(false);
+        })
+    }
+    const reject=()=>{
+        setRejectLoader(true)
+        setBtnDisabled(true)
+
+        ServiceClient.rejectCourseRequest(requestId).then((success)=>{
+            setSuccess(true);
+            setTimeout(()=>{
+                setSuccess(false);
+            },2000);
+            setRejectLoader(false);
+            navigation('/requests');
+        }).catch((error)=>{
+            setServerError(error);
+            setBtnDisabled(false);
+            setRejectLoader(false)
+        })
+    }
+
+
+    return (
+        <>
+            <EventHandler
+                success={success}
+                errors={errors}
+                serverError={serverError}
+                closeErrorMessage={(data)=>{if(data===true){setErrors([])}}}
+            />
+            <AreYouSure
+                name={areYouSureName}
+                answer={(name) => functionControl(name)}
+                transitionProp={areYouSureTransitionProp}
+            />
+        <div>
+            <form className="FlexForm">
+                <div className="form-title distance"><h2>{t('form.titles.main')}</h2><h2>{requestDetails.status}</h2></div>
+                <div className="form-items">
+                    {requestDetails.parent_info ?
+                        <>
+                            {requestDetails.parent_info.length ? requestDetails.parent_info.map(i =>
+                                    <>
+                                        <div className="form-title"><h2>{t('form.titles.parentInfo')}</h2></div>
+                                        <div className="form-children">
+                                            <label>{t('form.fname')}</label>
+                                            <input type="text" readOnly value={i.first_name}/>
+                                        </div>
+                                        <div className="form-children">
+                                            <label>{t('form.lname')}</label>
+                                            <input type="text" readOnly value={i.last_name}/>
+                                        </div>
+                                        <div className="form-children">
+                                            <label>{t('form.email')}</label>
+                                            <input type="email" readOnly value={i.email}/>
+                                        </div>
+                                    </>
+                                ) :
+                                <>
+                                    <div className="form-title"><h2>{t('form.titles.parentInfo')}</h2></div>
+                                    <div className="form-children">
+                                        <label>{t('form.fname')}</label>
+                                        <input type="text" readOnly value={requestDetails.parent_info.first_name}/>
+                                    </div>
+                                    <div className="form-children">
+                                        <label>{t('form.lname')}</label>
+                                        <input type="text" readOnly value={requestDetails.parent_info.last_name}/>
+                                    </div>
+                                    <div className="form-children">
+                                        <label>{t('form.email')}</label>
+                                        <input type="email" readOnly value={requestDetails.parent_info.email}/>
+                                    </div>
+                                </>
+                            }</>
+                        : null}
+
+                    <div className="form-title"><h2>{t('form.titles.childInfo')}</h2></div>
+                    <div className="form-children">
+                        <label>{t('form.fname')}</label>
+                        <input type="text" readOnly value={requestDetails.child_info.first_name}/>
+                    </div>
+                    <div className="form-children">
+                        <label>{t('form.lname')}</label>
+                        <input type="text" readOnly value={requestDetails.child_info.last_name}/>
+                    </div>
+                    <div className="form-children">
+                        <label>{t('form.birthday')}</label>
+                        <input type="date" readOnly value={requestDetails.child_info.birthday}/>
+                    </div>
+                    <div className="form-title"><h2>{t('form.titles.requestInfo')}</h2></div>
+                    <div className="form-children">
+                        <label>{t('form.courseName')}</label>
+                        <input type="text" readOnly value={requestDetails.course_names_and_langs[0].name}/>
+                    </div>
+                    <div className="form-children">
+                        <label>{t('form.numberOfLesson')}</label>
+                        <input type="text" readOnly value={requestDetails.number_of_lessons}/>
+                    </div>
+                    <div className="form-children">
+                        <label>{t('form.notice')}</label>
+                        <textarea readOnly value={requestDetails.notice}/>
+                    </div>
+                    {isTeacher ?
+                        <div className="form-button-container">
+                            {!acceptLoader ?
+                                <button
+                                    type='button'
+                                    disabled={btndisabled}
+                                    onClick={(e) => [
+                                        setAreYouSureName('reject'),
+                                        setAreYouSureTransitionProp(true)
+                                    ]}
+                                    className={readOnly ? 'formBtnDisabled' : 'btn formButton'}>
+                                    {t('form.buttons.reject')} <IoMdClose className='btn-icon'/>
+                                </button> :
+                                <span className='loader schoolDetails'></span>
+                            }
+                            {!rejectLoader ?
+                                <button
+                                    type='button'
+                                    disabled={btndisabled}
+                                    onClick={(e) => [
+                                        setAreYouSureName('accept'),
+                                        setAreYouSureTransitionProp(true)
+                                    ]}
+                                    className={readOnly ? 'formBtnDisabled' : 'btn formButton'}>
+                                    {t('form.buttons.accept')} <FaCheck className='btn-icon'/>
+                                </button> :
+                                <span className='loader schoolDetails'></span>
+                            }
+                        </div>
+                        : null}
+                </div>
+            </form>
+
+        </div>
+        </>
+    );
+};
+
+export default RequestDetails;
