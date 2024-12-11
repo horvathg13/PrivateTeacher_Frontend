ffa34fab2f5bd7e4e882495d15c13e12fece7066
hgabor13 <horvathgabor338@gmail.com>
2024-12-11 11:14:59 +0100

    css refactored + localization

--- src/CommonComponents/AreYouSure/areyousure.jsx
+++ src/CommonComponents/AreYouSure/areyousure.jsx
@@ -2,19 +2,21 @@ import { useRef } from 'react';
 import '../common.css';
 import { CSSTransition } from 'react-transition-group';
 import '../../transitions.css'
+import {useTranslation} from "react-i18next";
         
 const AreYouSure = ({label, name, answer, transitionProp}) => {
     const nodeRef=useRef(null)
+    const {t}=useTranslation();
     return (
         <CSSTransition nodeRef={nodeRef} in={transitionProp} classNames="fade" timeout={500} mountOnEnter unmountOnExit>
             <div className="are-you-sure-bcg" ref={nodeRef}>
                 <div className="are-yousure-main">
                     <div className="title">
-                        <h1>{label ? label : 'Are You Sure?'}</h1>
+                        <h3>{label ? label : t('areYouSure.title')}</h3>
                     </div>
                     <div className="answers flex">
-                        <button onClick={()=>{answer(name)}} >Yes</button>
-                        <button onClick={()=>{answer('cancel')}} >No</button>
+                        <button onClick={()=>{answer(name)}} >{t('areYouSure.yes')}</button>
+                        <button onClick={()=>{answer('cancel')}} >{t('areYouSure.no')}</button>
                     </div>
                 </div>
             </div>
