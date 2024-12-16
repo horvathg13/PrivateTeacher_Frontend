e9772aa9974d68766c0ff838ce3d152ac85c48ae
hgabor13 <horvathgabor338@gmail.com>
2024-12-16 07:54:28 +0100

    Protected Routes modified

--- src/ProtectedRoutes.js
+++ src/ProtectedRoutes.js
@@ -1,10 +1,11 @@
 import {Outlet, Navigate} from 'react-router-dom';
+import {useContext} from "react";
+import {UserContext} from "./Context/UserContext";
 
 const ProtectedRoutes=(props)=>{
-
-    if(localStorage.getItem('token')){
+    const {status}=useContext(UserContext);
+    if(localStorage.getItem('token') && status !== 'BANNED'){
         return props.children
-            
         
     }else{
         return(
