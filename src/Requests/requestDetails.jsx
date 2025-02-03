36e934cf4bf65ee1da7ad57c60d0a77ae3e09d07
hgabor13 <horvathgabor338@gmail.com>
2025-02-03 12:09:57 +0100

    modified

--- src/Requests/requestDetails.jsx
+++ src/Requests/requestDetails.jsx
@@ -8,6 +8,9 @@ import ServiceClient from "../ServiceClient";
 import AreYouSure from "../CommonComponents/AreYouSure/areyousure";
 import {UserContext} from "../Context/UserContext";
 import Notification from "../CommonComponents/Notifications/notification";
+import {MdPersonRemove} from "react-icons/md";
+import {IoExit} from "react-icons/io5";
+import TeachindDaySelect from "../CommonComponents/TeachingDay/teachindDaySelect";
 
 const RequestDetails = () => {
     /*Translation*/
@@ -15,12 +18,13 @@ const RequestDetails = () => {
     const {t: a}=useTranslation();
 
     /*Data*/
-    const requestDetails=useLoaderData();
-    const requestId=useParams();
+    const [requestDetails,teachingDays]=useLoaderData();
+    const {requestId}=useParams();
     const {roles}=useContext(UserContext);
     const [isTeacher, setIsTeacher]=useState();
     const [message, setMessage]=useState();
-
+    const [startDate, setStartDate]=useState();
+    const [teachingDayDetails, setTeachingDayDetails]=useState([]);
     /*Event handle*/
     const [errors, setErrors]=useState([]);
     const [success, setSuccess]=useState(false);
@@ -29,6 +33,7 @@ const RequestDetails = () => {
     /*Loader */
     const [acceptLoader, setAcceptLoader]=useState(false);
     const [rejectLoader, setRejectLoader]=useState(false);
+    const [removeLoader, setRemoveLoader]=useState(false);
 
     /*Navigation */
     const navigation=useNavigate();
@@ -55,6 +60,9 @@ const RequestDetails = () => {
         if(name==='reject'){
             reject();
         }
+        if(name==='remove'){
+            removeStudent();
+        }
         setAreYouSureTransitionProp(false);
     }
     const accept=()=>{
@@ -64,7 +72,7 @@ const RequestDetails = () => {
         setErrors([]);
         setServerError([]);
 
-        ServiceClient.acceptCourseRequest(requestId,message).then((success)=>{
+        ServiceClient.acceptCourseRequest(requestId,message,startDate, teachingDayDetails).then((success)=>{
             setSuccess(true);
             setTimeout(()=>{
                 setSuccess(false);
@@ -83,7 +91,7 @@ const RequestDetails = () => {
         setErrors([]);
         setServerError([]);
 
-        ServiceClient.rejectCourseRequest(requestId,message).then((success)=>{
+        ServiceClient.rejectCourseRequest(requestId).then((success)=>{
             setSuccess(true);
             setTimeout(()=>{
                 setSuccess(false);
@@ -97,28 +105,42 @@ const RequestDetails = () => {
         })
     }
 
+    const removeStudent=()=>{
+        setRemoveLoader(true);
+
+        ServiceClient.removeStudentFromCourse(requestId, requestDetails?.child_info?.id, message).then((success)=>{
+            setRemoveLoader(false);
+            navigation("/requests")
+        }).catch(error=>{
+            setServerError(error);
+            setRemoveLoader(false);
+        });
+    }
 
     return (
         <>
-            <EventHandler
-                success={success}
-                errors={errors}
-                serverError={serverError}
-                closeErrorMessage={(data)=>{if(data===true){setErrors([])}}}
-            />
-            <AreYouSure
-                name={areYouSureName}
-                answer={(name) => functionControl(name)}
-                transitionProp={areYouSureTransitionProp}
-            />
-            <Notification
-                active={active}
-            />
+        <EventHandler
+            success={success}
+            errors={errors}
+            serverError={serverError}
+            closeErrorMessage={(data) => {
+                if (data === true) {
+                    setErrors([])
+                }
+            }}
+        />
+        <AreYouSure
+            name={areYouSureName}
+            answer={(name) => functionControl(name)}
+            transitionProp={areYouSureTransitionProp}
+        />
         <div>
             <form className="FlexForm">
-                <div className="form-title distance"><h2>{t('form.titles.main')}</h2><h2>{a(`enums.${requestDetails.status}`)}</h2></div>
-                { requestDetails.teacher_justification ? <textarea className="justification-container" readOnly value={requestDetails.teacher_justification}
-                           placeholder={t('form.justification-placeholder')}/>:null}
+                <div className="form-title distance"><h2>{t('form.titles.main')}</h2>
+                    <h2>{a(`enums.${requestDetails.status}`)}</h2></div>
+                {requestDetails.teacher_justification ?
+                    <textarea className="justification-container" readOnly value={requestDetails.teacher_justification}
+                              placeholder={t('form.justification-placeholder')}/> : null}
                 <div className="form-items">
                     {requestDetails.parent_info ?
                         <>
@@ -140,7 +162,8 @@ const RequestDetails = () => {
                                     <div className="form-collapse">
                                         <div className="form-children">
                                             <label>{t('form.name')}</label>
-                                            <input type="text" readOnly value={requestDetails.parent_info.first_name.concat(" ", requestDetails.parent_info.last_name)}/>
+                                            <input type="text" readOnly
+                                                   value={requestDetails.parent_info.first_name.concat(" ", requestDetails.parent_info.last_name)}/>
                                         </div>
                                         <div className="form-children">
                                             <label>{t('form.email')}</label>
@@ -149,38 +172,82 @@ const RequestDetails = () => {
                                     </div>
                                 </>
                             }</>
-                        : null}
-
+                        : null
+                    }
                     <div className="form-title"><h3>{t('form.titles.childInfo')}</h3></div>
                     <div className="form-collapse">
                         <div className="form-children">
                             <label>{t('form.name')}</label>
-                            <input type="text" readOnly value={requestDetails.child_info.first_name.concat(" ", requestDetails.child_info.last_name)}/>
+                            <input type="text" readOnly
+                                   value={requestDetails.child_info.first_name.concat(" ", requestDetails.child_info.last_name)}/>
                         </div>
                         <div className="form-children">
                             <label>{t('form.birthday')}</label>
                             <input type="date" readOnly value={requestDetails.child_info.birthday}/>
                         </div>
                     </div>
-                    <div className="form-title"><h3>{t('form.titles.requestInfo')}</h3></div>
-                    <div className="form-children">
-                        <label>{t('form.courseName')}</label>
-                        <input type="text" readOnly value={requestDetails.course_names_and_langs[0].name}/>
-                    </div>
-                    <div className="form-children">
-                        <label>{t('form.numberOfLesson')}</label>
-                        <input type="text" readOnly value={requestDetails.number_of_lessons}/>
-                    </div>
-                    <div className="form-children">
-                        <label>{t('form.notice')}</label>
-                        <textarea readOnly value={requestDetails.notice}/>
-                    </div>
+                    {!requestDetails.terminationDetails ?
+                        <>
+                            <div className="form-title"><h3>{t('form.titles.requestInfo')}</h3></div>
+                            <div className="form-children">
+                                <label>{t('form.courseName')}</label>
+                                <input type="text" readOnly value={requestDetails.course_names_and_langs[0].name}/>
+                            </div>
+                            <div className="form-children">
+                                <label>{t('form.numberOfLesson')}</label>
+                                <input type="text" readOnly value={requestDetails.number_of_lessons}/>
+                            </div>
+                            <div className="form-children">
+                                <label>{t('form.start')}</label>
+                                <input type="date" readOnly value={requestDetails.requested_start_date}/>
+                            </div>
+
+                            <div className="form-children">
+                                <label>{t('form.notice')}</label>
+                                <textarea readOnly value={requestDetails.notice}/>
+                            </div>
+                        </>
+                        :
+                        <>
+                            <div className="form-title"><h3>{t('form.titles.requestInfo')}</h3></div>
+                            <div className="form-children">
+                                <label>{t('form.courseName')}</label>
+                                <input type="text" readOnly value={requestDetails.course_names_and_langs[0].name}/>
+                            </div>
+                            <div className="form-children">
+                                <label>{t('form.start')}</label>
+                                <input type="date" readOnly value={requestDetails.start_date}/>
+                            </div>
+                            <div className="form-children">
+                                <label>{t('form.termination_start')}</label>
+                                <input type="date" readOnly value={requestDetails.terminationDetails.from}/>
+                            </div>
+                            <div className="form-children">
+                                <label>{t('form.notice')}</label>
+                                <textarea readOnly value={requestDetails.terminationDetails.message}/>
+                            </div>
+                        </>
+                    }
                     {isTeacher && requestDetails.status === 'UNDER_REVIEW' ?
                         <>
                             <div className="form-title"><h2>{t('form.titles.answer')}</h2></div>
                             <div className="form-children">
+                                <label>{t('form.start')}</label>
+                                <input type="date" onChange={(e) => setStartDate(e.target.value)}/>
+                            </div>
+                            <div className="form-children">
+                                <label>{t('form.teaching_day')}</label>
+                                <TeachindDaySelect
+                                    disabled={false}
+                                    teachingDays={teachingDays}
+                                    teachingDaysEmit={(data) => setTeachingDayDetails(data)}
+                                />
+                            </div>
+                            <div className="form-children">
                                 <label>{t('form.justification')}</label>
-                                <textarea required onChange={(e)=>{setMessage(e.target.value)} }/>
+                                <textarea required onChange={(e) => {
+                                    setMessage(e.target.value)
+                                }}/>
                             </div>
                             <div className="form-button-container">
                                 {!rejectLoader ?
@@ -211,10 +278,10 @@ const RequestDetails = () => {
                                 }
                             </div>
                         </>
-                        : null}
-                </div>
+                        :null
+                    }
+                 </div>
             </form>
-
         </div>
         </>
     );
