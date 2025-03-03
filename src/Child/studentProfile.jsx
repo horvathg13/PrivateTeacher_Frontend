7d500d2c363a877647990635a8dfeef5b2e8c2cb
hgabor13 <horvathgabor338@gmail.com>
2025-03-03 10:28:07 +0100

    termination by teacher function dev

--- src/Child/studentProfile.jsx
+++ src/Child/studentProfile.jsx
@@ -1,9 +1,12 @@
 import React, {useContext, useEffect, useState} from 'react';
 import EventHandler from "../EventHandler/eventhandler";
-import {useLoaderData, useParams} from "react-router-dom";
+import {useLoaderData, useNavigate, useParams} from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import {ComponentTitleContext} from "../Context/UserContext";
 import courseProfile from "../Course/courseProfile";
+import {FaArrowCircleRight} from "react-icons/fa";
+import AreYouSure from "../CommonComponents/AreYouSure/areyousure";
+import ServiceClient from "../ServiceClient";
 
 const StudentProfile = () => {
 
@@ -17,6 +20,8 @@ const StudentProfile = () => {
     /*Data*/
     const {setTitle,setBreadcrumbs}=useContext(ComponentTitleContext);
     const{courseId}=useParams();
+    const [studentCourseId, setStudentCourseId]=useState(childProfile?.id);
+    const [terminationDate, setTerminationDate]=useState();
 
     /*Event handle*/
     const [errors, setErrors]=useState([]);
@@ -26,6 +31,47 @@ const StudentProfile = () => {
 
     /*Loader */
     const [loader, setLoader]=useState(true);
+    const [btnLoader, setBtnLoader]=useState(false);
+
+    /*Btn Control*/
+    const [btndisabled, setBtndisabled] = useState(false);
+
+    /*AreYouSure*/
+    const [areYouSureTransitionProp, setAreYouSureTransitionProp]=useState();
+    const [areYouSureName, setAreYouSureName]=useState('');
+
+    /*Navigation*/
+    const navigate=useNavigate();
+
+    const functionControl=(name)=>{
+        if(name==='termination'){
+            terminationByTeacher();
+        }
+        setAreYouSureTransitionProp(false);
+        setBtnLoader(false)
+        setBtndisabled(false)
+    }
+
+    const terminationByTeacher=()=>{
+        setErrors([]);
+        setServerError([]);
+        if(terminationDate){
+            ServiceClient.terminationCourseByTeacher(studentCourseId, terminationDate).then((success)=>{
+                setSuccess(true);
+                setTimeout(()=>{
+                    setSuccess(false)
+                },1500);
+                navigate(`/course/${courseId}/students`)
+            }).catch(error=>{
+                if(errors.length){
+                    setErrors([])
+                }
+                setServerError(error)
+            })
+        }else{
+            setErrors([t('validator.date')]);
+        }
+    }
 
     useEffect(() => {
         if(childProfile){
@@ -69,6 +115,11 @@ const StudentProfile = () => {
                         setErrors([]);
                     }
                 }}
+            />
+            <AreYouSure
+                name={areYouSureName}
+                answer={(name) => functionControl(name)}
+                transitionProp={areYouSureTransitionProp}
             />
             <div>
                 {!loader ? (
@@ -124,33 +175,78 @@ const StudentProfile = () => {
                             ) : null}
                         </div>
                         <div className="form-items">
+                            <div className="form-title"><h2>{t('course-profile.titles.profile')}</h2></div>
+                            <div className="form-children">
+                                <label>{t('course-profile.form.start')}</label>
+                                <input type="date" value={childProfile.start_date} readOnly/>
+                            </div>
+                            <div className="form-children">
+                                <label>{t('course-profile.form.end')}</label>
+                                <input type="date" value={childProfile.end_date} readOnly/>
+                            </div>
+                            <div className="form-children">
+                                <label>{t('course-profile.form.language')}</label>
+                                <input type="text" value={a(`enums.${childProfile.language}`)} readOnly/>
+                            </div>
+
                             <div className="form-title">
                                 <h2>{t('course-profile.titles.timetable')}</h2>
                             </div>
                             <div className="table-main-container">
                                 <table>
                                     <thead>
-                                        <tr>
-                                            <th>{t('course-profile.timetable.header.day')}</th>
-                                            <th>{t('course-profile.timetable.header.from')}</th>
-                                            <th>{t('course-profile.timetable.header.to')}</th>
-                                        </tr>
+                                    <tr>
+                                        <th>{t('course-profile.timetable.header.day')}</th>
+                                        <th>{t('course-profile.timetable.header.from')}</th>
+                                        <th>{t('course-profile.timetable.header.to')}</th>
+                                    </tr>
                                     </thead>
                                     <tbody>
-                                    {childProfile.teaching_days.length>0 ? childProfile.teaching_days.map((e,i) =>
+                                    {childProfile.teaching_days.length > 0 ? childProfile.teaching_days.map((e, i) =>
                                         <tr key={i} className="disableSelection">
                                             <td>{a(`enums.${e.teaching_day}`)}</td>
                                             <td>{e.from.slice(0, 5)}</td>
                                             <td>{e.to.slice(0, 5)}</td>
                                         </tr>
-                                    ):  <>
-                                            <tr>
-                                                <td colSpan={3} className="no-school">{a('empty-table')}</td>
-                                            </tr>
-                                        </>
+                                    ) : <>
+                                        <tr>
+                                            <td colSpan={3} className="no-school">{a('empty-table')}</td>
+                                        </tr>
+                                    </>
                                     }
                                     </tbody>
                                 </table>
+                            </div>
+                            <div className="form-items">
+                                <div className="form-title">
+                                    <h2>{t('course-profile.titles.terminationByTeacher')}</h2>
+                                </div>
+
+                                <div className="form-items">
+                                    <div className="form-children">
+                                        <label>{t('form.start-date')}</label>
+                                        <input type="date" required value={terminationDate} onChange={(e) => {
+                                            setTerminationDate(e.target.value)
+                                        }}/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="form-button-container">
+                                {!btnLoader ?
+                                    <button
+                                        type='button'
+                                        disabled={btndisabled}
+                                        onClick={(e) => [
+                                            setBtnLoader(true),
+                                            setBtndisabled(true),
+                                            setAreYouSureName("termination"),
+                                            setAreYouSureTransitionProp(true)
+                                        ]}
+                                        className='btn formButton'>
+                                        {t('button.termination')}
+                                    </button> :
+                                    <span className='loader schoolDetails'></span>
+                                }
                             </div>
                         </div>
                     </form>
