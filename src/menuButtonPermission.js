7312046df9cd5e785b5d9a04e77125b1bea04271
hgabor13 <horvathgabor338@gmail.com>
2025-03-14 11:35:58 +0100

    redirection bug fixed

--- src/menuButtonPermission.js
+++ src/menuButtonPermission.js
@@ -1,17 +1,25 @@
 
 class menuButtonPermission{
-static hasAccess=(menuItems, userRole, hasAccessMessages, hasAccessRequests)=>{
+    static hasAccess=(menuItems, userRole, hasAccessMessages, hasAccessRequests, hasChild)=>{
 
-    if(userRole.some(r=>r === "Parent") || userRole.some(r=>r === "Teacher")){
-            let filterMenuItemsByRole=menuItems.filter(menuItem=>menuItem.role.some(r=>userRole.includes(r)));
-            let accessToMessages=!hasAccessMessages ? filterMenuItemsByRole.filter(m=>m.name !== "messages") : filterMenuItemsByRole;
-            let accessToRequests=!hasAccessRequests ? filterMenuItemsByRole.filter(r=>r.name !== "requests") : filterMenuItemsByRole;
+        if(userRole.some(r=>r === "Parent") || userRole.some(r=>r === "Teacher")){
+                let filterMenuItemsByRole=menuItems.filter(menuItem=>menuItem.role.some(r=>userRole.includes(r)));
+                let accessToMessages=!hasAccessMessages ? filterMenuItemsByRole.filter(m=>m.name !== "messages") : filterMenuItemsByRole;
+                let accessToRequests=!hasAccessRequests ? filterMenuItemsByRole.filter(r=>r.name !== "requests") : filterMenuItemsByRole;
 
-            let final = [...accessToMessages, ...accessToRequests];
-            return final.map(item => item).filter((value, index, self) => self.indexOf(value) !== index)
+                let final = [...accessToMessages, ...accessToRequests];
+                let uniqueFinal = final.map(item => item).filter((value, index, self) => self.indexOf(value) !== index)
 
-    }
-    return menuItems.filter(menuItem=>menuItem.role.some(r=>userRole.includes(r)));
+                let redirectURL = !hasChild ? "/child/create" : "/home";
+                return {menu:uniqueFinal, url:redirectURL}
+
+        }
+        let menu = menuItems.filter(menuItem=>menuItem.role.some(r=>userRole.includes(r)));
+        let redirectURL='';
+        if(menu.length === 1){
+            redirectURL = "/users/list";
+        }
+        return {menu: menu, url: redirectURL || "/home"}
 
     }
 }
